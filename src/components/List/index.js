850f835be90b9a2a973b28ed62ed47dab03f04bf
luis_salazar85 <lsalazar@tbxnet.com>
2017-12-24 13:01:14 -0300

    Generacion Landing Component

---
+++ src/components/List/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListComponent extends Component {
+     render() {
+         return(
+             <div className="list-content">
+
+             </div>
+         )
+     }
+}
+
+export default ListComponent;
